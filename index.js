e5f9a654a85aada59867381222843adf17d79a93
Restu Wahyu Saputra <restuwahyusaputra@MacBook-Pro-Restu.local>
2024-10-19 19:06:40 +0700

    Publish files

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Redis } = require('ioredis')
+
+class RedisClient {
+	config() {
+		return new Redis({
+			host: 'localhost',
+			port: 6379,
+			// tls: true
+		})
+	}
+
+	setCacheData(key, data) {
+		return this.config().set(key, data)
+	}
+
+	getCacheData(key) {
+		return this.config().get(key)
+	}
+}
+
+const redis = new RedisClient()
+redis.setCacheData("name", "max cavalera").then(console.log).catch(console.error)
+redis.getCacheData("name").then(console.log).catch(console.error).finally(() => process.exit(0))
